923b88c8a59eeb186875f36b8ffb80c34ae1fdf6
Daniel Medeiros <dmedeiros1209@gmail.com>
2024-02-11 23:10:17 -0300

    finalização da mudança para o principal.java

--- src/main/java/br/com/aplicativo/filmesmatch/FilmesmatchApplication.java
+++ src/main/java/br/com/aplicativo/filmesmatch/FilmesmatchApplication.java
@@ -1,8 +1,5 @@
 package br.com.aplicativo.filmesmatch;
-import io.github.cdimascio.dotenv.Dotenv;
-import br.com.aplicativo.filmesmatch.model.DadosSerie;
-import br.com.aplicativo.filmesmatch.service.ConsumoApi;
-import br.com.aplicativo.filmesmatch.service.ConverteDados;
+import br.com.aplicativo.filmesmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,21 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FilmesmatchApplication implements CommandLineRunner {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FilmesmatchApplication.class, args);
-	}
+        public static void main(String[] args) {
+            SpringApplication.run(FilmesmatchApplication.class, args);
+        }
 
-    @Override
-    public void run(String... args) throws Exception {
-        Dotenv dotenv = Dotenv.load();
-        String key = dotenv.get("API_KEY");
-        ConsumoApi consumoApi = new ConsumoApi();
-        System.out.println(consumoApi.obterDados("https://www.omdbapi.com/?t=The+sopranos&plot=full&apikey=" + key));
-        var json = consumoApi.obterDados("https://www.omdbapi.com/?t=The+sopranos&plot=full&apikey=" + key);
-        ConverteDados converteDados = new ConverteDados();
-        DadosSerie serie = converteDados.obterDados(json, DadosSerie.class);
-        System.out.println(serie);
+        @Override
+        public void run(String... args) throws Exception {
+            Principal principal = new Principal();
+            principal.exibeMenu();
+        }
     }
-
-
-}
