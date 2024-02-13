9848e7fe8282f196432b0574963ff0de376890d1
Daniel Medeiros <dmedeiros1209@gmail.com>
2024-02-12 22:11:50 -0300

    modelo dados das series

--- src/main/java/br/com/aplicativo/filmesmatch/model/DadosSerie.java
+++ src/main/java/br/com/aplicativo/filmesmatch/model/DadosSerie.java
@@ -3,11 +3,12 @@ package br.com.aplicativo.filmesmatch.model;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import java.time.LocalDate;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosSerie(@JsonAlias("Title") String titulo,
-                         @JsonAlias("Released") String lancamento,
                          @JsonAlias("totalSeasons") Integer totalTemporadas,
-                         @JsonAlias("imdbRating") float Rating,
-                         @JsonAlias("Poster") String poster ) {
+                         @JsonAlias("imdbRating") String avaliacao,
+                         @JsonAlias("Genre") String genero,
+                         @JsonAlias("Actors") String atores,
+                         @JsonAlias("Poster") String poster,
+                         @JsonAlias("Plot") String sinopse) {
 }
