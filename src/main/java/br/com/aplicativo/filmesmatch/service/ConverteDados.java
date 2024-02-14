9916cfc97a6b73e12ea2cfc656602080d8a3eeb6
Daniel Medeiros <dmedeiros1209@gmail.com>
2024-02-14 18:34:05 -0300

    Serviço para converter os dados

--- src/main/java/br/com/aplicativo/filmesmatch/service/ConverteDados.java
+++ src/main/java/br/com/aplicativo/filmesmatch/service/ConverteDados.java
@@ -1,11 +1,18 @@
 package br.com.aplicativo.filmesmatch.service;
 
-import tools.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.annotation.*;
 
-public class ConverteDados {
+public class ConverteDados implements IConverteDados {
     private ObjectMapper mapper = new ObjectMapper();
 
-    public <T> T obterDados(String json, Class<T> classe){
-        return mapper.readValue(json, classe);
+    @Override
+    public <T> T obterDados(String json, Class<T> classe) {
+        try {
+            return mapper.readValue(json, classe);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
