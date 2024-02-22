c6673aa89c11484659e0f13f2336c4f5f2c5527c
Daniel Medeiros <dmedeiros1209@gmail.com>
2024-02-22 17:50:47 -0300

    Organização para o SerieRepository

--- src/main/java/br/com/aplicativo/filmesmatch/FilmesmatchApplication.java
+++ src/main/java/br/com/aplicativo/filmesmatch/FilmesmatchApplication.java
@@ -1,5 +1,7 @@
 package br.com.aplicativo.filmesmatch;
 import br.com.aplicativo.filmesmatch.principal.Principal;
+import br.com.aplicativo.filmesmatch.repositoy.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,13 +9,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FilmesmatchApplication implements CommandLineRunner {
 
+        @Autowired
+        private SerieRepository serieRepository;
+
         public static void main(String[] args) {
             SpringApplication.run(FilmesmatchApplication.class, args);
         }
 
         @Override
         public void run(String... args) throws Exception {
-            Principal principal = new Principal();
+            Principal principal = new Principal(serieRepository);
             principal.exibeMenu();
         }
     }
