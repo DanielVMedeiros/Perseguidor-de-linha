959d8f6de1538daa389b57d900587a80d6870c23
Daniel Medeiros <dmedeiros1209@gmail.com>
2024-03-05 18:17:07 -0300

    Criação de uma consulta para buscar pelo título

--- src/main/java/br/com/aplicativo/filmesmatch/repositoy/SerieRepository.java
+++ src/main/java/br/com/aplicativo/filmesmatch/repositoy/SerieRepository.java
@@ -3,5 +3,8 @@ package br.com.aplicativo.filmesmatch.repositoy;
 import br.com.aplicativo.filmesmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SerieRepository extends JpaRepository<Serie, Integer> {
+    Optional<Serie> findByTituloContainingIgnoreCase(String titulo);
 }
