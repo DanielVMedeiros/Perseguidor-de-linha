9eeb6ab1eceeb2433b8458081bb3b9553ce8897c
Daniel Medeiros <dmedeiros1209@gmail.com>
2024-03-07 19:27:07 -0300

    Adicionando uma buscar por ator ao repositório

--- src/main/java/br/com/aplicativo/filmesmatch/repositoy/SerieRepository.java
+++ src/main/java/br/com/aplicativo/filmesmatch/repositoy/SerieRepository.java
@@ -3,8 +3,11 @@ package br.com.aplicativo.filmesmatch.repositoy;
 import br.com.aplicativo.filmesmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SerieRepository extends JpaRepository<Serie, Integer> {
     Optional<Serie> findByTituloContainingIgnoreCase(String titulo);
+
+    List<Serie> findByAtoresContainingIgnoreCase(String nomeAtor);
 }
