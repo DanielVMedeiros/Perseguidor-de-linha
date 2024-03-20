f118e0d4aa0261db34b632975c185097c4ddfe65
Daniel Medeiros <dmedeiros1209@gmail.com>
2024-03-19 22:09:11 -0300

    mudando o inicio da aplicação para subir servidor

--- src/main/java/br/com/aplicativo/filmesmatch/FilmesmatchApplication.java
+++ src/main/java/br/com/aplicativo/filmesmatch/FilmesmatchApplication.java
@@ -1,24 +1,12 @@
 package br.com.aplicativo.filmesmatch;
-import br.com.aplicativo.filmesmatch.principal.Principal;
-import br.com.aplicativo.filmesmatch.repositoy.SerieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FilmesmatchApplication implements CommandLineRunner {
-
-        @Autowired
-        private SerieRepository serieRepository;
+public class FilmesmatchApplication {
 
         public static void main(String[] args) {
             SpringApplication.run(FilmesmatchApplication.class, args);
         }
 
-        @Override
-        public void run(String... args) throws Exception {
-            Principal principal = new Principal(serieRepository);
-            principal.exibeMenu();
-        }
     }
