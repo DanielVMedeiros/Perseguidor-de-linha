d74ddeca9db4522fd5e658a602d5a318d6ce94c4
Daniel Medeiros <dmedeiros1209@gmail.com>
2024-03-22 20:07:11 -0300

    Finalizando o primeiro GET

--- src/main/java/br/com/aplicativo/filmesmatch/controller/SerieController.java
+++ src/main/java/br/com/aplicativo/filmesmatch/controller/SerieController.java
@@ -1,6 +1,7 @@
 package br.com.aplicativo.filmesmatch.controller;
 
 
+import br.com.aplicativo.filmesmatch.dto.SerieDTO;
 import br.com.aplicativo.filmesmatch.model.Serie;
 import br.com.aplicativo.filmesmatch.repositoy.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,14 +9,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
     @Autowired
-    private SerieRepository serieRepository;
+    private SerieRepository repository;
 
     @GetMapping("/series")
-    public List<Serie> getSerie() {
-        return serieRepository.findAll();
+    public List<SerieDTO> getSerie() {
+        List<SerieDTO> serie = repository.findAll()
+                .stream()
+                .map(s -> new SerieDTO(s.getId(),s.getTitulo(),s.getTotalTemporadas(), s.getAvaliacao(),s.getGenero(),s.getAtores(),s.getPoster(),s.getSinopse()))
+                .collect(Collectors.toList());
+        return serie;
     }
 }
