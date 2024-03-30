eebfee546197a7b72132dcc9b7f0c9ae6fb22665
Daniel Medeiros <dmedeiros1209@gmail.com>
2024-03-29 22:17:36 -0300

    Criação do get para buscar as top 5 melhores séries

--- src/main/java/br/com/aplicativo/filmesmatch/controller/SerieController.java
+++ src/main/java/br/com/aplicativo/filmesmatch/controller/SerieController.java
@@ -2,26 +2,28 @@ package br.com.aplicativo.filmesmatch.controller;
 
 
 import br.com.aplicativo.filmesmatch.dto.SerieDTO;
-import br.com.aplicativo.filmesmatch.model.Serie;
-import br.com.aplicativo.filmesmatch.repositoy.SerieRepository;
+import br.com.aplicativo.filmesmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
+
     @Autowired
-    private SerieRepository repository;
+    private SerieService serieService;
 
     @GetMapping("/series")
     public List<SerieDTO> getSerie() {
-        List<SerieDTO> serie = repository.findAll()
-                .stream()
-                .map(s -> new SerieDTO(s.getId(),s.getTitulo(),s.getTotalTemporadas(), s.getAvaliacao(),s.getGenero(),s.getAtores(),s.getPoster(),s.getSinopse()))
-                .collect(Collectors.toList());
-        return serie;
+        return serieService.getSeries();
     }
+
+    @GetMapping("/series/top5")
+    public List<SerieDTO> getTopSerie() {
+        return serieService.getTopSeries();
+    }
+
+
 }
