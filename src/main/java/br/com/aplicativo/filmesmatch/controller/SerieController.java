dd5e1b532effb31924d45115fe6e8e3a61bdd350
Daniel Medeiros <dmedeiros1209@gmail.com>
2024-03-31 19:12:26 -0300

    Adicionando o RequestMapping para organizar o controller

--- src/main/java/br/com/aplicativo/filmesmatch/controller/SerieController.java
+++ src/main/java/br/com/aplicativo/filmesmatch/controller/SerieController.java
@@ -5,22 +5,24 @@ import br.com.aplicativo.filmesmatch.dto.SerieDTO;
 import br.com.aplicativo.filmesmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("series")
 public class SerieController {
 
     @Autowired
     private SerieService serieService;
 
-    @GetMapping("/series")
+    @GetMapping
     public List<SerieDTO> getSerie() {
         return serieService.getSeries();
     }
 
-    @GetMapping("/series/top5")
+    @GetMapping("/top5")
     public List<SerieDTO> getTopSerie() {
         return serieService.getTopSeries();
     }
